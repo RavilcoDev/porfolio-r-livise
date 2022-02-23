f3f201e79d5c3c1cb2f455012abdd413a77d5586
Rafael Livise <r.livise@hotmail.com>
2022-02-23 09:21:52 -0500

    chore:[update] webpack.dev add devServer

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -49,4 +49,10 @@ module.exports = {
         }),
         new Dotenv()
     ],
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        compress: true,
+        historyApiFallback: true,
+        port: 3006,
+      },
 };
